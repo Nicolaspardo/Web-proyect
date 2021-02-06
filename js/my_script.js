2fd3a5c830c49cef983f06f1e5702e7084813858
nicolaspardo <nicolaspardo95@gmail.com>
2021-02-06 20:15:57 -0300

    Nueva version

---
+++ js/my_script.js
@@ -0,0 +1,17 @@
+
+
+
+//Boxslider
+$('.bxslider').bxSlider({
+  auto: true,
+  pager: true,
+  adaptiveHeight: false,
+  hideControlOnEnd: false,
+  controls:false,
+  responsive:true,
+});
+
+
+
+
+
